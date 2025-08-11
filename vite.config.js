23b3f20e37d27b3dc8b85e4b1357b903bd0d885a
Neli Mintcheva <nelimintcheva@rightpoint.com>
2025-08-11 11:08:15 -0400

    Add base and esbuild

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,15 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react(), tailwindcss()],
+  esbuild: {
+    loader: 'jsx',
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      './runtimeConfig': './runtimeConfig.browser',
     },
   },
 });
